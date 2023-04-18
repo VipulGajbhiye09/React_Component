e8e7082d6b2d09083a1af5d143a18bac661da8e4
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-18 23:58:07 +0530

    Create List.jsx

---
+++ List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3/li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
